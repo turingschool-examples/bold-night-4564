2cf1388200d8e758fef3d0db79143cfa64219935
deebot10 <dee1jhill@gmail.com>
2021-07-26 10:08:28 -0600

    Create test for Story2

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe 'Mechanic Show' do
   before(:each) do
     @mechanic = Mechanic.create!(name: 'Dee', years_of_experience: 4)  
     
-    @ride_1 = @mechanic_1.rides.create!(name: 'Whiplash', thrill_rating: 8, open: true)
-    @ride_2 = @mechanic_1.rides.create!(name: 'Tea Cups', thrill_rating: 10, open: false)
-    @ride_3 = @mechanic_1.rides.create!(name: 'Halfpipe', thrill_rating: 6, open: true)
+    @ride_1 = @mechanic.rides.create!(name: 'Whiplash', thrill_rating: 8, open: true)
+    @ride_2 = @mechanic.rides.create!(name: 'Tea Cups', thrill_rating: 10, open: false)
+    @ride_3 = @mechanic.rides.create!(name: 'Halfpipe', thrill_rating: 6, open: true)
 
     visit "/mechanics/#{@mechanic.id}"
   end
@@ -23,6 +23,10 @@ RSpec.describe 'Mechanic Show' do
       
       expect(page).to have_content(@mechanic.name)
       expect(page).to have_content(@mechanic.years_of_experience)
+
+      expect(page).to have_content(@ride_1.name)
+      expect(page).to have_content(@ride_2.name)
+      expect(page).to have_content(@ride_3.name)
     end   
   end  
 end
