9a212848aa58624ecf2aa5490283beed980ae5ea
Matt Toensing <matthew.toensing@gmail.com>
2021-07-26 08:33:26 -0700

    Add model test for average mechanic experience

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe Mechanic do
     it { should have_many(:mechanic_rides) }
     it { should have_many(:rides).through(:mechanic_rides)}
   end
+
+  describe 'class methods' do
+    describe ':average_experience' do
+      it 'shows the average experience for all mechanics' do
+        mechanic1 = create(:mechanic, years_of_experience: 6)
+        mechanic2 = create(:mechanic, years_of_experience: 5)
+        mechanic3 = create(:mechanic, years_of_experience: 2)
+
+        expect(Mechanic.average_experience).to eq(5)
+      end
+    end
+  end
 end
