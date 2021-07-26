d993bccdefce3de1dfb260ab4f97c96a47fc7665
Matt Kragen <mattdkragen@gmail.com>
2021-07-26 10:31:54 -0700

    Correct tests for new migrations

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'Mechanics show page' do
   before :each do
+    @park_1 = AmusementPark.create!(name: 'Blips and Chitz', admission_price: 30.0)
     @mech_1 = Mechanic.create!(name: 'Todd', years_of_experience: 6)
-    @ride_1 = Ride.create!(name: 'The Whirly Dirly', thrill_rating: 10, open: true)
-    @ride_2 = Ride.create!(name: 'Frankenpants', thrill_rating: 20, open: false)
-    @ride_3 = Ride.create!(name: 'Vomit World', thrill_rating: 30, open: true)
+    @ride_1 = @park_1.rides.create!(name: 'The Whirly Dirly', thrill_rating: 10, open: true)
+    @ride_2 = @park_1.rides.create!(name: 'Frankenpants', thrill_rating: 20, open: false)
+    @ride_3 = @park_1.rides.create!(name: 'Vomit World', thrill_rating: 30, open: true)
     @ride_mech_1 = RideMechanic.create!(mechanic: @mech_1, ride: @ride_1)
     @ride_mech_2 = RideMechanic.create!(mechanic: @mech_1, ride: @ride_2)
     @ride_mech_3 = RideMechanic.create!(mechanic: @mech_1, ride: @ride_3)
@@ -36,7 +37,7 @@ RSpec.describe 'Mechanics show page' do
 
   it 'can add a ride to the show page' do
     visit "/mechanics/#{@mech_1.id}"
-    ride_4 = Ride.create!(name: 'Wiggle Bomb', thrill_rating: 15, open: true)
+    ride_4 = @park_1.rides.create!(name: 'Wiggle Bomb', thrill_rating: 15, open: true)
     
     fill_in "Ride Id:", with: "#{ride_4.id}"
     click_button "Submit"
