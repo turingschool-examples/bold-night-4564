e92ac9e80fe18c8f7d257f0d17e3d9beca39237d
deebot10 <dee1jhill@gmail.com>
2021-07-26 09:21:02 -0600

    Create test for ride, mechanic, maintenance model

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -1,5 +1,8 @@
 require "rails_helper"
 
 RSpec.describe Mechanic do
-  
+   describe 'relationships' do
+    it {should have_many(maintenances)}  
+    it {should have_many(rides).through(maintenances)}  
+  end    
 end
