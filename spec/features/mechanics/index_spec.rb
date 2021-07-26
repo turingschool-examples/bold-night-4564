a7d2f0fad61cc9b2a626fb2fbff10b33ab0a7fff
Elliot Olbright <elliotolbright@gmail.com>
2021-07-26 09:35:17 -0600

    finish user story 1

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -2,10 +2,10 @@ require "rails_helper"
 
 RSpec.describe 'Mechanics Index Page' do 
   before :each do 
-    @mechanic1 = Mechanic.create!(name: "Jose", years_experience: 10)
-    @mechanic2 = Mechanic.create!(name: "John", years_experience: 12)
-    @mechanic3 = Mechanic.create!(name: "Julie", years_experience: 14)
-    @mechanic4 = Mechanic.create!(name: "James", years_experience: 16)
+    @mechanic1 = Mechanic.create!(name: "Jose", years_of_experience: 10)
+    @mechanic2 = Mechanic.create!(name: "John", years_of_experience: 12)
+    @mechanic3 = Mechanic.create!(name: "Julie", years_of_experience: 14)
+    @mechanic4 = Mechanic.create!(name: "James", years_of_experience: 16)
 
     visit '/mechanics' 
   end 
@@ -19,5 +19,9 @@ RSpec.describe 'Mechanics Index Page' do
     expect(page).to have_content(@mechanic2.name)
     expect(page).to have_content(@mechanic3.name)
     expect(page).to have_content(@mechanic4.name)
+  end 
+
+  it 'can display avg yrs of experience' do 
+    expect(page).to have_content("Average Years of Experience: 13")
   end 
 end
--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -6,4 +6,16 @@ RSpec.describe Mechanic do
     it { should have_many(:rides).through(:maintenences) }
   end 
 
+  describe 'methods' do 
+    describe '::average_years' do 
+      it 'can average all the years of mechanics expo' do 
+        mechanic1 = Mechanic.create!(name: "Jose", years_of_experience: 10)
+        mechanic2 = Mechanic.create!(name: "John", years_of_experience: 12)
+        mechanic3 = Mechanic.create!(name: "Julie", years_of_experience: 14)
+        mechanic4 = Mechanic.create!(name: "James", years_of_experience: 16)
+
+        expect(Mechanic.average_years).to eq(13)
+      end 
+    end
+  end
 end
