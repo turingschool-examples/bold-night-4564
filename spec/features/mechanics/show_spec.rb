1b5ae7bbf937e19b48da02ced411a80fcf6701f0
deebot10 <dee1jhill@gmail.com>
2021-07-26 10:31:39 -0600

    Create Ride Model test

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -12,13 +12,6 @@ RSpec.describe 'Mechanic Show' do
   end
 
   describe 'visit' do
-    #     Story 2 - Mechanic Show Page
-
-    # As a user,
-    # When I visit a mechanic show page
-    # I see their name, years of experience, and the names of all rides they’re working on
-    # And I only see rides that are open
-    # And the rides are listed by thrill rating in descending order (most thrills first)  
     it 'displays a mechanics an the attributes' do
       
       expect(page).to have_content(@mechanic.name)
@@ -34,6 +27,30 @@ RSpec.describe 'Mechanic Show' do
         expect(@ride_1.name).to appear_before(@ride_3.name)  
         expect(page).to_not have_content(@ride_2.name) 
       end
+    end
+    
+      #     Story 3 - Add a Ride to a Mechanic
+
+      # As a user,
+      # When I go to a mechanics show page
+      # I see a form to add a ride to their workload
+      # When I fill in that field with an id of an existing ride and hit submit
+      # I’m taken back to that mechanic's show page
+      # And I see the name of that newly added ride on this mechanics show page
+
+      # Ex:
+      # Mechanic: Kara Smith
+      # Years of Experience: 11
+
+      # Current rides they’re working on:
+      # The Frog Hopper
+      # Fahrenheit
+      # The Kiss Raise
+
+      # Add a ride to workload:
+      # Ride Id: _pretend_this_is_a_textfield_
+      # Submit
+    it '' do
     end
   end  
 end
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -4,5 +4,20 @@ RSpec.describe Ride do
   describe 'relationships' do
     it {should have_many(:maintenances)}  
     it {should have_many(:mechanics).through(:maintenances)}  
-  end  
+  end
+
+  before(:each) do
+    @mechanic = Mechanic.create!(name: 'Dee', years_of_experience: 4)  
+    
+    @ride_1 = @mechanic.rides.create!(name: 'Whiplash', thrill_rating: 8, open: true)
+    @ride_2 = @mechanic.rides.create!(name: 'Tea Cups', thrill_rating: 10, open: false)
+    @ride_3 = @mechanic.rides.create!(name: 'Halfpipe', thrill_rating: 6, open: true)
+  end
+
+  describe 'class methods' do
+    it 'displays rides that are open in order' do
+
+      expect(Ride.open_thrill).to eq([@ride_1, @ride_3])
+    end  
+  end
 end
