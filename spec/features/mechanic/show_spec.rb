1f6c46a736842029fec1cbd767f338e781ffdf70
Brett Jones <bhjones4545@gmail.com>
2021-07-26 10:27:07 -0600

    Create mechanic show page test

---
+++ spec/features/mechanic/show_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'mechanic show page' do
+  it 'lists name, years of exp, and names of rides they work on' do
+    mech_1 = Mechanic.create!(name: 'Bubbles', years_of_experience: 5)
+    mech_2 = Mechanic.create!(name: 'Ricky', years_of_experience: 15)
+
+    ride_1 = Ride.create!(name: "Steve French", thrill_rating: 5, open: true)
+    ride_2 = Ride.create!(name: "Z-town", thrill_rating: 9, open: true)
+    ride_3 = Ride.create!(name: "Z-town", thrill_rating: 6, open: false)
+
+
+    maint_1 = Maintenence.create!(mechanic: mech_1, ride: ride_1)
+    maint_2 = Maintenence.create!(mechanic: mech_1, ride: ride_2)
+
+    visit "/mechanics/#{mech_1.id}"
+
+    expect(page).to have_content(mech_1.name)
+    expect(page).to have_content(mech_1.years_of_experience)
+    expect(page).to have_content("Currently Maintaining:")
+    expect(ride_2.name).to appear_before(ride_1.name)
+    expect(page).to_not have_content(ride_3.name)
+  end
+end
