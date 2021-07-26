953d959cdefd466ce9f0a9e25a42712767e2fd9d
Alex Klick <aklick12@gmail.com>
2021-07-26 09:47:43 -0600

    passes US 1

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -2,7 +2,6 @@ class MechanicsController < ApplicationController
   def index
     @mechanics = Mechanic.all
     @average_exp = Mechanic.av_exp
-    binding.pry
   end
   def show
     @mechanic = Mechanic.find(params[:id])
