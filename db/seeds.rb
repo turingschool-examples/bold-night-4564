7254d42cd7ac406b7daaea541ca82da95599f24e
Jacob Martinez <jacobmarcareer@gmail.com>
2021-07-26 10:55:45 -0600

    Make some initial seeds to fill tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Mechanic.destroy_all
+
+@mechanic_1 = Mechanic.create!(
+  name: 'Michael',
+  years_of_experience: 8
+)
+@mechanic_2 = Mechanic.create!(
+  name: 'Dwight',
+  years_of_experience: 7
+)
+@mechanic_3 = Mechanic.create!(
+  name: 'Jim',
+  years_of_experience: 4
+)
+@mechanic_4 = Mechanic.create!(
+  name: 'Pam',
+  years_of_experience: 10
+)
