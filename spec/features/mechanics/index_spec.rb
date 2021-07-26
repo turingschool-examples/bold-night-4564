34d010aae9639e6591c1929da342dec0536868da
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-26 09:55:42 -0600

    Build route for mechanic index and test for display of attributes

---
+++ spec/features/mechanics/index_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+RSpec.describe 'the mechanics index' do
+  before(:each) do
+    @mechanic_1 = Mechanic.create(name: 'Nick McIntyre', years_of_experience: 10)
+    @mechanic_2 = Mechanic.create(name: 'Lee Hopper', years_of_experience: 5)
+    @mechanic_3 = Mechanic.create(name: 'Cody Eldredge', years_of_experience: 7)
+  end
+
+  it 'lists all mechanic names and years of experience' do
+    visit '/mechanics'
+
+    expect(page).to have_content('All Mechanics')
+
+    within("#mechanic-#{@mechanic_1.id}") do
+      expect(page).to have_content(@mechanic_1.name)
+      expect(page).to have_content(@mechanic_1.years_of_experience)
+    end
+
+    within("#mechanic-#{@mechanic_2.id}") do
+      expect(page).to have_content(@mechanic_2.name)
+      expect(page).to have_content(@mechanic_2.years_of_experience)
+    end
+
+    within("#mechanic-#{@mechanic_2.id}") do
+      expect(page).to have_content(@mechanic_2.name)
+      expect(page).to have_content(@mechanic_2.years_of_experience)
+    end
+  end
+
+  it 'displays the average years of experience for all mechanics' do
+    visit '/mechanics'
+  end
+end
