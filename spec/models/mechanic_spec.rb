3204bab7b38d148175cf53122d5a5c222c2dd0e6
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-26 09:55:21 -0600

    Build test and then method for average experience

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -5,4 +5,18 @@ RSpec.describe Mechanic do
     it { should have_many(:ride_mechanics) }
     it { should have_many(:rides).through(:ride_mechanics)}
   end
+
+  before(:each) do
+    @mechanic_1 = Mechanic.create(name: 'Nick McIntyre', years_of_experience: 10)
+    @mechanic_2 = Mechanic.create(name: 'Lee Hopper', years_of_experience: 5)
+    @mechanic_3 = Mechanic.create(name: 'Cody Eldredge', years_of_experience: 7)
+  end
+
+  describe 'class methods' do
+    describe '.average_experience' do
+      it 'returns the average years of experience for all mechanics' do
+        expect(Mechanic.average_experience).to eq(7.3)
+      end
+    end
+  end
 end
