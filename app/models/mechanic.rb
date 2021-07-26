8e00870ce7fdb389aae85c68b7381908249c24bc
Carina Sweet <carina.sweet06@gmail.com>
2021-07-26 11:20:09 -0600

    Complete user story 2

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -5,4 +5,8 @@ class Mechanic < ApplicationRecord
   def self.avg_years_of_experience
     average(:years_of_experience).to_f
   end
+
+  def open_rides_desc_by_thrill_rating
+    rides.where(open: true).order(thrill_rating: :desc)
+  end
 end
--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -15,10 +15,36 @@ RSpec.describe Mechanic do
       @mechanic_4 = Mechanic.create!(name: 'Holly', years_of_experience: 15)
       @mechanic_5 = Mechanic.create!(name: 'Mason', years_of_experience: 18)
     end
-    
+    #user story 1
     describe '::avg_years_of_experience' do
       it 'returns the average years of experience for all mechanics' do
         expect(Mechanic.avg_years_of_experience).to eq(20.6)
+      end
+    end
+  end
+
+  describe 'instance methods' do
+    before :each do
+      @mechanic_1 = Mechanic.create!(name: 'Robert', years_of_experience: 27)
+
+      @ride_1 = Ride.create!(name: 'Big Thunder', thrill_rating: 8, open: true)
+      @ride_2 = Ride.create!(name: 'Mine Train', thrill_rating: 2, open: true)
+      @ride_3 = Ride.create!(name: 'Space Mountain', thrill_rating: 7, open: false)
+      @ride_4 = Ride.create!(name: 'The Barnstormer', thrill_rating: 9, open: true)
+      @ride_5 = Ride.create!(name: 'Wildlife Express', thrill_rating: 5, open: false)
+      @ride_6 = Ride.create!(name: 'Laugh Floor', thrill_rating: 6, open: true)
+
+      RideMechanic.create!(mechanic: @mechanic_1, ride: @ride_1)
+      RideMechanic.create!(mechanic: @mechanic_1, ride: @ride_2)
+      RideMechanic.create!(mechanic: @mechanic_1, ride: @ride_3)
+      RideMechanic.create!(mechanic: @mechanic_1, ride: @ride_4)
+      RideMechanic.create!(mechanic: @mechanic_1, ride: @ride_5)
+      RideMechanic.create!(mechanic: @mechanic_1, ride: @ride_6)
+    end
+    #user story 2
+    describe '#open_rides_desc_by_thrill_rating' do
+      it 'returns rides open for the mechanic in desc order by thrill rating' do
+        expect(@mechanic_1.open_rides_desc_by_thrill_rating).to eq([@ride_4, @ride_1, @ride_6, @ride_2])
       end
     end
   end
