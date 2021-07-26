bf9373c327c85792bca3f31c4f61ae8addbea87c
Jacob Piland <piland.jacob@gmail.com>
2021-07-26 12:15:23 -0500

    add work order controller and create method with form to add on show page, refactor routes to handroll

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :mechanics
+  # resources :mechanics
+
+  get '/mechanics', to: 'mechanics#index'
+  get '/mechanics/:id', to: 'mechanics#show'
+  post '/mechanics/:mechanic_id/work_orders', to: 'work_orders#create'
 end
