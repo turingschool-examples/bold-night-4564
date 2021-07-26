3a2db74b5b3b8cfe782e8734ed7dfeac3b5380f3
Matt Kragen <mattdkragen@gmail.com>
2021-07-26 09:02:15 -0700

    Create route for controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   get '/mechanics', to: 'mechanics#index'
   get '/mechanics/:id', to: 'mechanics#show'
-  
+  post '/mechanics/:mechanic_id/rides', to: 'ride_mechanics#create'
 end
