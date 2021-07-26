dd47eed5fa8685d0c92975f53b9db9ed6ee791d1
Alex Klick <aklick12@gmail.com>
2021-07-26 09:33:48 -0600

    test setup for US 1

---
+++ spec/features/mechanics/show_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'the amechanics index page' do
+  before(:each) do
+  
+
+  end
+  # As a user,
+  # When I visit a mechanic show page
+  # I see their name, years of experience, and the names of all rides they’re working on
+  # And I only see rides that are open
+  # And the rides are listed by thrill rating in descending order (most thrills first)
+  it 'has the name, years exp and the rides theyre working on, which are sorted by thrill rating' do
+
+  end
+
+  #   As a user,
+  # When I go to a mechanics show page
+  # I see a form to add a ride to their workload
+  # When I fill in that field with an id of an existing ride and hit submit
+  # I’m taken back to that mechanic's show page
+  # And I see the name of that newly added ride on this mechanics show page
+  it 'has a form to add a ride, when filled out, takes back to the mech show page where the new ride ' do
+
+  end
+end
