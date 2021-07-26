c6b7cf9371a492243fdd77841ac36207ba5ec5c9
Antonio King <antonio.j.king@gmail.com>
2021-07-26 09:25:49 -0600

    Setup ride and mechanic associations; incomplete

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -1,5 +1,9 @@
 require "rails_helper"
 
 RSpec.describe Mechanic do
-  
+  describe "relationships" do
+    it {should have_many :maintenances}
+    it {should have_many(:rides).through(:maintenances)}
+  end
+
 end
