a0cf7546a2d9441374915ce01bea8aa99bb2c938
Marla Schulz <marlaa.schulz@gmail.com>
2021-07-26 09:49:29 -0600

    Add average years method

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,4 +1,8 @@
 class Mechanic < ApplicationRecord
   has_many:assignments
   has_many:rides, through: :assignments
+
+  def self.average_years
+    average(:years_of_experience)
+  end
 end
--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe Mechanic do
     it {should have_many(:rides).through(:assignments)}
   end
 
+  describe 'Class methods' do
+    it 'can calculate average years' do
+      lily = Mechanic.create!(name: 'Lily', years_of_experience: 10)
+      mike = Mechanic.create!(name: 'Mike', years_of_experience: 8)
+
+      expect(Mechanic.average_years).to eq(9)
+    end
+  end
 end
