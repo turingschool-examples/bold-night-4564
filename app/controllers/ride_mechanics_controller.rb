63ecb198898f27ce1dedef2a8901b2712df16e56
Matt Kragen <mattdkragen@gmail.com>
2021-07-26 09:17:17 -0700

    Fix error in find_by method

--- app/controllers/ride_mechanics_controller.rb
+++ app/controllers/ride_mechanics_controller.rb
@@ -1,6 +1,6 @@
-class RideMechanicsController < ApplicationCOntroller
+class RideMechanicsController < ApplicationController
   def create
-    mechanic = Mechanic.find(params[:id])
+    mechanic = Mechanic.find(params[:mechanic_id])
     ride = Ride.find_by(id: params[:ride_id])
     RideMechanic.create(ride: ride, mechanic: mechanic)
     
