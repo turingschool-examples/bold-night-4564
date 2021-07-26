fa855d7d1e2c3c5380c863ff051cabf6da21c8d0
Carina Sweet <carina.sweet06@gmail.com>
2021-07-26 10:51:29 -0600

    Complete user story 1

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,4 +1,8 @@
 class Mechanic < ApplicationRecord
   has_many :ride_mechanics
   has_many :rides, through: :ride_mechanics
+
+  def self.avg_years_of_experience
+    average(:years_of_experience).to_f
+  end
 end
--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -7,4 +7,19 @@ RSpec.describe Mechanic do
   it { should have_many(:rides).through(:ride_mechanics)}
   end
 
+  describe 'class methods' do
+    before :each do
+      @mechanic_1 = Mechanic.create!(name: 'Robert', years_of_experience: 27)
+      @mechanic_2 = Mechanic.create!(name: 'Brian', years_of_experience: 20)
+      @mechanic_3 = Mechanic.create!(name: 'Rachel', years_of_experience: 23)
+      @mechanic_4 = Mechanic.create!(name: 'Holly', years_of_experience: 15)
+      @mechanic_5 = Mechanic.create!(name: 'Mason', years_of_experience: 18)
+    end
+    
+    describe '::avg_years_of_experience' do
+      it 'returns the average years of experience for all mechanics' do
+        expect(Mechanic.avg_years_of_experience).to eq(20.6)
+      end
+    end
+  end
 end
