14c0446faa6854413d40593580e08256dbeda21b
Matt Toensing <matthew.toensing@gmail.com>
2021-07-26 10:15:31 -0700

    Add expectations for showing order number of rides

--- spec/features/amusement_parks/show_spec.rb
+++ spec/features/amusement_parks/show_spec.rb
@@ -4,9 +4,6 @@ RSpec.describe 'amusement park show page' do
   describe 'contents' do
     it 'displays the name and admission price for the park' do
       amusement_park = AmusementPark.create!(name: 'Six Flags', admission_price: 25)
-      ride1 = Ride.create!(name: 'Ferris Wheel', thrill_rating: 3, open: true, amusement_park_id: amusement_park.id)
-      ride2 = Ride.create!(name: 'Roller Coaster', thrill_rating: 6, open: true, amusement_park_id: amusement_park.id)
-      ride3 = Ride.create!(name: 'Merry Go Round', thrill_rating: 9, open: false, amusement_park_id: amusement_park.id)
 
       visit "/amusementparks/#{amusement_park.id}"
 
@@ -24,6 +21,9 @@ RSpec.describe 'amusement park show page' do
 
       expect(ride1.name).to appear_before(ride3.name)
       expect(ride3.name).to appear_before(ride2.name)
+      expect(page).to have_content("1. #{ride1.name}")
+      expect(page).to have_content("2. #{ride3.name}")
+      expect(page).to have_content("3. #{ride2.name}")
     end
 
     it 'displays the average thrill rating of the rides at the park' do
@@ -34,7 +34,7 @@ RSpec.describe 'amusement park show page' do
 
       visit "/amusementparks/#{amusement_park.id}"
       rides = amusement_park.rides
-      save_and_open_page
+
       expect(page).to have_content("Average Thrill Rating of Rides: #{rides.average_thrill_rating}/10")
     end
   end
