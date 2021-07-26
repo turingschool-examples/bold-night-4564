c24649396ad11c88fff79356aa21b1c049bcbc21
Jacob Piland <piland.jacob@gmail.com>
2021-07-26 11:15:56 -0500

    add relationship tests to models

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -1,5 +1,20 @@
 require "rails_helper"
 
 RSpec.describe Mechanic do
-  
+  describe 'relationships' do
+    it {should have_many :work_orders}
+    it {should have_many(:rides).through(:work_orders)}
+  end
+
+  describe 'class methods' do
+    describe '::avg_experience' do
+      it 'can return the average years of experience for all mechanics' do
+        tom = Mechanic.create!(name: "Tom", years_of_experience: 6)
+        john = Mechanic.create!(name: "John", years_of_experience: 4)
+        bob = Mechanic.create!(name: "Bob", years_of_experience: 5)
+
+        expect(Mechanic.avg_experience).to eq(5)
+      end
+    end
+  end
 end
