20bf13beaca76040d998ed0c217517f991cca260
Brett Jones <bhjones4545@gmail.com>
2021-07-26 11:00:15 -0600

    Story 2 complete

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -2,4 +2,9 @@ class MechanicsController < ApplicationController
   def index
     @mechanics = Mechanic.all
   end
+
+  def show
+    @mechanics = Mechanic.find(params[:id])
+    @rides = @mechanics.order_by_open_thrill
+  end
 end
