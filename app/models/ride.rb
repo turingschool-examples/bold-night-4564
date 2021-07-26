b2688829ef6f5a77368fd0a68b7d6b25f836c673
Matt Toensing <matthew.toensing@gmail.com>
2021-07-26 08:54:30 -0700

    Add model test and method for open rides

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,8 @@
 class Ride < ApplicationRecord
   has_many :mechanic_rides
   has_many :mechanics, through: :mechanic_rides
+
+  def self.open_rides
+    Ride.where(open: true)
+  end
 end
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -5,4 +5,18 @@ RSpec.describe Ride do
     it { should have_many(:mechanic_rides) }
     it { should have_many(:mechanics).through(:mechanic_rides) }
   end
+
+  describe 'class methods' do
+    describe ':open_rides' do
+      it 'returns only rides that are open or true' do
+        ride1 = Ride.create!(name: 'Ferris Wheel', thrill_rating: 3, open: true)
+        ride2 = Ride.create!(name: 'Roller Coaster', thrill_rating: 6, open: true)
+        ride3 = Ride.create!(name: 'Merry Go Round', thrill_rating: 6, open: false)
+
+        expected = [ride1, ride2]
+
+        expect(Ride.open_rides).to eq(expected)
+      end
+    end
+  end
 end
