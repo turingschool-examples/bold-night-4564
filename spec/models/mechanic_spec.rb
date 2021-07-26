382343ec8b5d5ef34ac439d1e3812713409db98c
Carina Sweet <carina.sweet06@gmail.com>
2021-07-26 10:31:15 -0600

    Add tests and model assosiations

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -1,5 +1,10 @@
 require "rails_helper"
 
 RSpec.describe Mechanic do
-  
+
+  describe 'Relationships' do
+  it { should have_many :ride_mechanics}
+  it { should have_many(:rides).through(:ride_mechanics)}
+  end
+
 end
