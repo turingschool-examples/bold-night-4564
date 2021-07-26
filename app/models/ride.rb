d11aacf53df9847f60d22353f6322be4fba82668
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-26 10:36:35 -0600

    Build test and then method for order rides by thrill

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,5 +1,8 @@
 class Ride < ApplicationRecord
   has_many :ride_mechanics
   has_many :mechanics, through: :ride_mechanics
-  
+
+  def self.order_by_thrill
+    order(thrill_rating: :desc)
+  end
 end
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -5,4 +5,19 @@ RSpec.describe Ride do
     it { should have_many(:ride_mechanics) }
     it { should have_many(:mechanics).through(:ride_mechanics)}
   end
+
+  before(:each) do
+    @ride_1 = Ride.create!(name: 'The Titan', thrill_rating: 10, open: false)
+    @ride_2 = Ride.create!(name: 'Batman', thrill_rating: 2, open: true)
+    @ride_3 = Ride.create!(name: 'Superman', thrill_rating: 9, open: true)
+    @ride_4 = Ride.create!(name: 'WonderWoman', thrill_rating: 5, open: true)
+  end
+
+  describe 'class methods' do
+    describe '.order_by_thrill' do
+      it 'orders the rides by their thrill rating in descending order' do
+        expect(Ride.order_by_thrill).to eq([@ride_1, @ride_3, @ride_4, @ride_2])
+      end
+    end
+  end
 end
