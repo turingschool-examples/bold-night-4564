53300ba560493d015e0b0cf5a0896a5c5a610c63
Matt Toensing <matthew.toensing@gmail.com>
2021-07-26 09:52:56 -0700

    Add route for amusement park show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get '/mechanics', to: 'mechanics#index'
   get '/mechanics/:id', to: 'mechanics#show'
   post '/mechanics/:id', to: 'mechanic_rides#create'
+
+  get '/amusementparks/:id', to: 'amusement_parks#show'
 end
