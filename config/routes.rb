e859dbc645deedc4aa914ac7335035df023ebc45
Michael Abbott <michael@mjabbottdesign.com>
2021-07-26 10:32:09 -0700

    Complete story 3

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
 
 get '/mechanics',     to: 'mechanics#index'
 get '/mechanics/:id', to: 'mechanics#show'
+
+post '/mechanics/:id/ride_mechanics', to: 'ride_mechanics#create'
 end
