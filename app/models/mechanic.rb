f8e05b812a7923b90088973eea8ef7150a181294
Marla Schulz <marlaa.schulz@gmail.com>
2021-07-26 10:09:18 -0600

    Add open rides method

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -5,4 +5,8 @@ class Mechanic < ApplicationRecord
   def self.average_years
     average(:years_of_experience)
   end
+
+  def open_rides
+    self.rides.where(open: true).order(thrill_rating: :desc)
+  end
 end
--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -14,4 +14,19 @@ RSpec.describe Mechanic do
       expect(Mechanic.average_years).to eq(9)
     end
   end
+
+  describe 'instance methods' do
+    it 'retrieves open rides in thrill order' do
+      lily = Mechanic.create!(name: 'Lily', years_of_experience: 10)
+
+      ride1 = Ride.create!(name: 'Twister', thrill_rating: 4, open: true)
+      ride2 = Ride.create!(name: 'Loopy', thrill_rating: 6, open: false)
+      ride3= Ride.create!(name: 'Choo Choo', thrill_rating: 2, open: true)
+      ride4= Ride.create!(name: 'Bolt', thrill_rating: 7, open: true)
+
+      lily.rides << [ride1, ride2, ride3, ride4]
+
+      expect(lily.open_rides).to eq([ride4, ride1, ride3])
+    end
+  end
 end
