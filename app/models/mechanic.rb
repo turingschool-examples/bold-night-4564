2b47ecbf1685e8a497b6d484588ceb3a1332ca6b
danembb <dbrophy720@gmail.com>
2021-07-26 12:04:06 -0600

    Final refactor for submission

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,4 +1,5 @@
 class Mechanic < ApplicationRecord
   has_many :ride_mechanics
-  has_many :rides, through: :ride_mechanics, -> { order(thrill_rating: :desc)}
+  has_many :rides, through: :ride_mechanics
+  # -> {order(thrill_rating: :desc)} for establishing descending order by thrill_rating
 end
