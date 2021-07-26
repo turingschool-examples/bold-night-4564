a9fbf38915fa76baac230a6fab3b757d0c2e98f0
Antonio King <antonio.j.king@gmail.com>
2021-07-26 09:33:58 -0600

    Setup associations; complete

---
+++ spec/models/ride_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe Ride do
+  describe "relationships" do
+    it {should have_many :maintenances}
+    it {should have_many(:mechanics).through(:maintenances)}
+  end
+
+end
