487ee6a5559f975c78a25a914c5c429e6770df87
Jason Knoll <jason.p.knoll@gmail.com>
2021-07-26 09:02:13 -0700

    Add model test for avg years

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe Mechanic do
     it  {should have_many :maintenances}
     it  {should have_many(:rides).through(:maintenances)}
   end
+
+  describe "class methods" do
+    before(:each) do
+      @bob = Mechanic.create!(name: "Bob", years_of_experience: 12)
+      @sarah = Mechanic.create!(name: "Sarah", years_of_experience: 11)
+    end
+    it "should show the average years experience for all mechanics" do
+      expect(Mechanic.average_years_experience).to eq(11.5)
+    end
+  end
 end
