a7f3d722dd1521595454ee4861c1ef96af77f8fa
Jacob Piland <piland.jacob@gmail.com>
2021-07-26 11:38:58 -0500

    add method order by thrill rating to ride model

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,8 @@
 class Ride < ApplicationRecord
   has_many :work_orders
   has_many :mechanics, through: :work_orders
+
+  def self.order_by_thrill_rating 
+    order(thrill_rating: :desc)
+  end
 end
