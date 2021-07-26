0f2d712fa1132e24b5729184f52b3268d9a77758
Jason Knoll <jason.p.knoll@gmail.com>
2021-07-26 09:18:09 -0700

    Add tests to list all rides in mechanic show page

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -4,6 +4,13 @@ RSpec.describe "Mechanics show page" do
   before(:each) do
     @bob = Mechanic.create!(name: "Bob", years_of_experience: 12)
 
+    @ride_1 = Ride.create!(name: "Sky Rocket", thrill_rating: 10, open: true)
+    @ride_2 = Ride.create!(name: "Splash Valley", thrill_rating: 3, open: true)
+    @ride_3 = Ride.create!(name: "Tunnel Vision", thrill_rating: 6, open: true)
+
+    @maintenance_1 = Maintenance.create!(ride: @ride_1, mechanic: @bob)
+    @maintenance_2 = Maintenance.create!(ride: @ride_2, mechanic: @bob)
+    @maintenance_3 = Maintenance.create!(ride: @ride_3, mechanic: @bob)
     visit "/mechanics/#{@bob.id}"
   end
 
@@ -12,7 +19,11 @@ RSpec.describe "Mechanics show page" do
     expect(page).to have_content(12)
   end
 
-  it "lists all the rides that they are working on" 
+  it "lists all the rides that they are working on" do
+    expect(page).to have_content("Sky Rocket")
+    expect(page).to have_content("Splash Valley")
+    expect(page).to have_content("Tunnel Vision")
+  end
 
 
   it "all rides are listed by thrill rating descending order"
