a1ea7f6b756e67e0c336e56a19fe6a8b93a0d3ac
Marla Schulz <marlaa.schulz@gmail.com>
2021-07-26 10:26:16 -0600

    Update mechanics show page to include add ride functionality

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -6,4 +6,12 @@ class MechanicsController < ApplicationController
   def show
     @mechanic = Mechanic.find(params[:id])
   end
+
+  def update
+    mechanic = Mechanic.find(params[:id])
+    ride = Ride.find(params[:ride_id])
+    mechanic.rides << ride
+
+    redirect_to "/mechanics/#{mechanic.id}"
+  end
 end
--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -24,5 +24,25 @@ RSpec.describe 'Mechanics Show Page' do
       expect(this).to appear_before(that)
       expect(page).to_not have_content('Loopy')
     end
+
+    it 'can add new ride to assignments' do
+      lily = Mechanic.create!(name: 'Lily', years_of_experience: 10)
+
+      ride1 = Ride.create!(name: 'Twister', thrill_rating: 4, open: true)
+      ride2 = Ride.create!(name: 'Loopy', thrill_rating: 6, open: false)
+      ride3= Ride.create!(name: 'Choo Choo', thrill_rating: 2, open: true)
+      ride4= Ride.create!(name: 'Bolt', thrill_rating: 7, open: true)
+
+      lily.rides << [ride1, ride2, ride3]
+
+      visit "/mechanics/#{lily.id}"
+
+      fill_in(:ride_id, with: ride4.id)
+      click_on('Submit')
+
+      expect(current_path).to eq("/mechanics/#{lily.id}")
+
+      expect(page).to have_content('Bolt')
+    end
   end
 end
