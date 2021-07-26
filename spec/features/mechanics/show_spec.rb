1d9127926a611794649cf0a0fcb8158f0a0d14a3
Marla Schulz <marlaa.schulz@gmail.com>
2021-07-26 10:13:08 -0600

    Upgrade testing

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'Mechanics Show Page' do
   describe 'visitor' do
+    let(:this) {'Twister'}
+    let(:that) {'Choo Choo'}
+
     it 'displays a mechanics attributes and their assignments' do
       lily = Mechanic.create!(name: 'Lily', years_of_experience: 10)
 
@@ -16,12 +19,10 @@ RSpec.describe 'Mechanics Show Page' do
       expect(page).to have_content('Lily')
       expect(page).to have_content('10')
 
-      let(:this) {'Twister'}
-      let(:that) {'Choo Choo'}
-
-      expect(page).to have_content('Ride Assignments:')
+      expect(page).to have_content('Ride Assignments')
 
       expect(this).to appear_before(that)
+      expect(page).to_not have_content('Loopy')
     end
   end
 end
