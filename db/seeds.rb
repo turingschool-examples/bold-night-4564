a6c499c65ca90f376a761d626a49c525fdfcf7d5
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-07-26 13:58:28 -0400

    Update seeds file, style amusementpark show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,25 @@
 @mechanic2 = Mechanic.create!(name: 'Walter White', years_of_experience: 4)
 
 @disney = AmusementPark.create!(name: 'Disney World', price: 125)
+@universal = AmusementPark.create!(name: 'Universal Studios', price: 115)
+@busch = AmusementPark.create!(name: 'Busch Gardens', price: 115)
+
 
 @ride1 = @disney.rides.create!(name: 'Ferris Wheel', thrill_rating: 4, open: true)
 @ride2 = @disney.rides.create!(name: 'Roller Coaster', thrill_rating: 10, open: true)
 @ride3 = @disney.rides.create!(name: 'Bombs Away', thrill_rating: 3, open: true)
 @ride4 = @disney.rides.create!(name: 'Carousel', thrill_rating: 1, open: true)
+
+@ride5 = @universal.rides.create!(name: 'Hulk', thrill_rating: 10, open: true)
+@ride6 = @universal.rides.create!(name: 'Harry Potter', thrill_rating: 8, open: true)
+@ride7 = @universal.rides.create!(name: 'Jaws', thrill_rating: 6, open: true)
+@ride8 = @universal.rides.create!(name: 'Car disaster', thrill_rating: 7, open: true)
+
+@ride9 = @busch.rides.create!(name: 'Montu', thrill_rating: 10, open: true)
+@ride10 = @busch.rides.create!(name: 'Sheikra', thrill_rating: 9, open: true)
+@ride11 = @busch.rides.create!(name: 'Kumba', thrill_rating: 8, open: true)
+@ride12 = @busch.rides.create!(name: 'River Rapids', thrill_rating: 4, open: true)
+
 
 @mechanic1.work_orders.create!(ride: @ride1)
 @mechanic1.work_orders.create!(ride: @ride2)
