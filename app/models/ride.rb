8379c558f0ff54432d611fff25bf9e4b86f2458d
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-26 10:26:39 -0600

    Build tests and then display for list of open rides for mechanic

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,5 @@
 class Ride < ApplicationRecord
   has_many :ride_mechanics
   has_many :mechanics, through: :ride_mechanics
+  
 end
---
+++ spec/features/mechanics/show_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.describe 'the mechanics index' do
+  before(:each) do
+    @mechanic_1 = Mechanic.create(name: 'Nick McIntyre', years_of_experience: 10)
+    @ride_1 = @mechanic_1.rides.create(name: 'The Titan', thrill_rating: 10, open: false)
+    @ride_2 = @mechanic_1.rides.create(name: 'Batman', thrill_rating: 2, open: true)
+    @ride_3 = @mechanic_1.rides.create(name: 'Superman', thrill_rating: 9, open: true)
+    @ride_4 = @mechanic_1.rides.create(name: 'WonderWoman', thrill_rating: 5, open: true)
+  end
+
+  it 'displays the mechanic attributes' do
+    visit "/mechanics/#{@mechanic_1.id}"
+
+    within("#attributes") do
+      expect(page).to have_content(@mechanic_1.name)
+      expect(page).to have_content(@mechanic_1.years_of_experience)
+    end
+  end
+
+  it 'displays the mechanics rides that are open' do
+    visit "/mechanics/#{@mechanic_1.id}"
+
+    expect(page).to_not have_content(@ride_1.name)
+
+    expect(page).to have_content(@ride_2.name)
+    expect(page).to have_content(@ride_3.name)
+    expect(page).to have_content(@ride_4.name)
+  end
+end
