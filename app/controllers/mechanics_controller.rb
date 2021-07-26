31ad8e7142a84f83cd26ba94dfd1cc7d4b647fa8
Matt Kragen <mattdkragen@gmail.com>
2021-07-26 08:44:59 -0700

    Add show method and order rides

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -3,4 +3,8 @@ class MechanicsController < ApplicationController
     @mechanics = Mechanic.all
   end
 
+  def show
+    @mechanic = Mechanic.find(params[:id])
+    @rides = @mechanic.rides.where(open: true).order(thrill_rating: :desc)
+  end
 end
