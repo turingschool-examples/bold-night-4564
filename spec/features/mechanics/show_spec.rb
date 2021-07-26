60b788b831289d61150c600868326537b73ca5f7
Matt Toensing <matthew.toensing@gmail.com>
2021-07-26 09:37:58 -0700

    Update tests to inlcude rides with amusement park id

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe 'mechanic show page' do
   describe 'contents' do
     it 'displays the mechanics name, years of experience, and names of rides they are working on' do
+      amusement_park = AmusementPark.create!(name: 'Six Flags', admission_price: 25)
       mechanic = create(:mechanic)
-      ride1 = Ride.create!(name: 'Ferris Wheel', thrill_rating: 3, open: true)
-      ride2 = Ride.create!(name: 'Roller Coaster', thrill_rating: 6, open: true)
+      ride1 = Ride.create!(name: 'Ferris Wheel', thrill_rating: 3, open: true, amusement_park_id: amusement_park.id)
+      ride2 = Ride.create!(name: 'Roller Coaster', thrill_rating: 6, open: true, amusement_park_id: amusement_park.id)
       mechanic_ride1 = MechanicRide.create(ride_id: ride1.id, mechanic_id: mechanic.id)
       mechanic_ride2 = MechanicRide.create(ride_id: ride2.id, mechanic_id: mechanic.id)
 
@@ -18,10 +19,11 @@ RSpec.describe 'mechanic show page' do
     end
 
     it 'only shows rides that are open and are listed by thrill rating in descending order' do
+      amusement_park = AmusementPark.create!(name: 'Six Flags', admission_price: 25)
       mechanic = create(:mechanic)
-      ride1 = Ride.create!(name: 'Ferris Wheel', thrill_rating: 3, open: true)
-      ride2 = Ride.create!(name: 'Roller Coaster', thrill_rating: 6, open: true)
-      ride3 = Ride.create!(name: 'Merry Go Round', thrill_rating: 9, open: false)
+      ride1 = Ride.create!(name: 'Ferris Wheel', thrill_rating: 3, open: true, amusement_park_id: amusement_park.id)
+      ride2 = Ride.create!(name: 'Roller Coaster', thrill_rating: 6, open: true, amusement_park_id: amusement_park.id)
+      ride3 = Ride.create!(name: 'Merry Go Round', thrill_rating: 9, open: false, amusement_park_id: amusement_park.id)
       mechanic_ride1 = MechanicRide.create(ride_id: ride1.id, mechanic_id: mechanic.id)
       mechanic_ride2 = MechanicRide.create(ride_id: ride2.id, mechanic_id: mechanic.id)
       mechanic_ride3 = MechanicRide.create(ride_id: ride3.id, mechanic_id: mechanic.id)
@@ -35,10 +37,11 @@ RSpec.describe 'mechanic show page' do
 
   describe 'page on form' do
     it 'displays a form to fill in an id of an existing ride to add to the mechanic' do
+      amusement_park = AmusementPark.create!(name: 'Six Flags', admission_price: 25)
       mechanic = create(:mechanic)
-      ride1 = Ride.create!(name: 'Ferris Wheel', thrill_rating: 3, open: true)
-      ride2 = Ride.create!(name: 'Roller Coaster', thrill_rating: 6, open: true)
-      ride3 = Ride.create!(name: 'Merry Go Round', thrill_rating: 9, open: true)
+      ride1 = Ride.create!(name: 'Ferris Wheel', thrill_rating: 3, open: true, amusement_park_id: amusement_park.id)
+      ride2 = Ride.create!(name: 'Roller Coaster', thrill_rating: 6, open: true, amusement_park_id: amusement_park.id)
+      ride3 = Ride.create!(name: 'Merry Go Round', thrill_rating: 9, open: true, amusement_park_id: amusement_park.id)
       mechanic_ride1 = MechanicRide.create(ride_id: ride1.id, mechanic_id: mechanic.id)
       mechanic_ride2 = MechanicRide.create(ride_id: ride2.id, mechanic_id: mechanic.id)
 
@@ -47,7 +50,7 @@ RSpec.describe 'mechanic show page' do
 
       fill_in 'ride_id', with: ride3.id
       click_on "Submit"
-      save_and_open_page
+      
       expect(current_path).to eq("/mechanics/#{mechanic.id}")
       expect(page).to have_content(ride3.name)
       expect(ride3.name).to appear_before(ride2.name)
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Ride do
   describe 'relationships' do
+    it { should belong_to(:amusement_park) }
     it { should have_many(:mechanic_rides) }
     it { should have_many(:mechanics).through(:mechanic_rides) }
   end
@@ -9,9 +10,10 @@ RSpec.describe Ride do
   describe 'class methods' do
     describe ':open_rides' do
       it 'returns only rides that are open or true' do
-        ride1 = Ride.create!(name: 'Ferris Wheel', thrill_rating: 3, open: true)
-        ride2 = Ride.create!(name: 'Roller Coaster', thrill_rating: 6, open: true)
-        ride3 = Ride.create!(name: 'Merry Go Round', thrill_rating: 6, open: false)
+        amusement_park = AmusementPark.create!(name: 'Six Flags', admission_price: 25)
+        ride1 = Ride.create!(name: 'Ferris Wheel', thrill_rating: 3, open: true, amusement_park_id: amusement_park.id)
+        ride2 = Ride.create!(name: 'Roller Coaster', thrill_rating: 6, open: true, amusement_park_id: amusement_park.id)
+        ride3 = Ride.create!(name: 'Merry Go Round', thrill_rating: 6, open: false, amusement_park_id: amusement_park.id)
 
         expected = [ride1, ride2]
 
@@ -21,9 +23,10 @@ RSpec.describe Ride do
 
     describe ':order_by_thrills' do
       it 'orders rides by thrill rating in descending order' do
-        ride1 = Ride.create!(name: 'Ferris Wheel', thrill_rating: 3, open: true)
-        ride2 = Ride.create!(name: 'Roller Coaster', thrill_rating: 9, open: true)
-        ride3 = Ride.create!(name: 'Merry Go Round', thrill_rating: 6, open: true)
+        amusement_park = AmusementPark.create!(name: 'Six Flags', admission_price: 25)
+        ride1 = Ride.create!(name: 'Ferris Wheel', thrill_rating: 3, open: true, amusement_park_id: amusement_park.id)
+        ride2 = Ride.create!(name: 'Roller Coaster', thrill_rating: 9, open: true, amusement_park_id: amusement_park.id)
+        ride3 = Ride.create!(name: 'Merry Go Round', thrill_rating: 6, open: true, amusement_park_id: amusement_park.id)
 
         expected = [ride2, ride3, ride1]
 
