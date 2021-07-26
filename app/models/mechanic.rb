6d75ba3b71884d0ec3103d125eecad6c3cc606cf
danembb <dbrophy720@gmail.com>
2021-07-26 12:01:55 -0600

    In progress test relationships for mechanic join tables

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,4 +1,4 @@
 class Mechanic < ApplicationRecord
   has_many :ride_mechanics
-  has_many :rides, through: :ride_mechanics
+  has_many :rides, through: :ride_mechanics, -> { order(thrill_rating: :desc)}
 end
--- spec/features/mechanic/show_spec.rb
+++ spec/features/mechanic/show_spec.rb
@@ -2,13 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Mechanic, type: :feature do
   before :each do
-    @mechanic1 = Mechanic.create!(name: 'Gertrude Robinson', years_of_experience: 50)
-    @mechanic2 = Mechanic.create!(name: 'Jonathan Sims', years_of_experience: 6)
+    @gertrude = Mechanic.create!(name: 'Gertrude Robinson', years_of_experience: 50)
+    @jonathan = Mechanic.create!(name: 'Jonathan Sims', years_of_experience: 6)
     @mechanics = Mechanic.all
-    @ride1 = Ride.create!(name: 'The Web', thrill_rating: 8, open: true)
-    @ride2 = Ride.create!(name: 'The Eye', thrill_rating: 2, open: true)
-    @ride3 = Ride.create!(name: 'The Flesh', thrill_rating: 4, open: true)
-    @ride4 = Ride.create!(name: 'The Dark', thrill_rating: 10, open: false)
+    @web = gertrude.rides.create!(name: 'The Web', thrill_rating: 8, open: true)
+    @eye = jonathan.rides.create!(name: 'The Eye', thrill_rating: 2, open: true)
+    @flesh = gertrude.rides.create!(name: 'The Flesh', thrill_rating: 4, open: false)
+    @dark = jonathan.rides.create!(name: 'The Dark', thrill_rating: 10, open: false)
+    RideMechanic.create!(ride: @web, mechanic: @gertrude)
+    RideMechanic.create!(ride: @web, mechanic: @jonathan)
+    RideMechanic.create!(ride: @flesh, mechanic: @gertrude)
+    RideMechanic.create!(ride: @eye, mechanic: @jonathan)
+    RideMechanic.create!(ride: @dark, mechanic: @jonathan)
   end
 
   # Story 2 - Mechanic Show Page
@@ -30,9 +35,12 @@ RSpec.describe Mechanic, type: :feature do
 
       xit 'can see the names of all rides the mechanic is working on' do
         visit '/mechanics/:id'
+
+        expect(page).to have_content(@ridemechanic1.ride)
       end
 
       xit 'can only see rides that are open' do
+        #this will be an if statement where the page will only display if it is true that the mechanics rides open value is true
         visit '/mechanics/:id'
       end
 
